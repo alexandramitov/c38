caa1dd0f9eec5dc7a113d086eabe92f5168ea8e7
alexandramitov <71188919+alexandramitov@users.noreply.github.com>
2020-12-16 14:05:01 -0500

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,76 @@
+class Game {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+  }
+
+    car1 = createSprite(200,100,30,30);
+    car2 = createSprite(200,200,30,30);
+    car3 = createSprite(200,300,30,30);
+    car4 = createSprite(200,400,30,30);
+
+    cars = [car1,car2,car3,car4];
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120, 100)
+    Player.getPlayerInfo();
+
+    if(allPlayers !== undefined){
+      var index = 0;
+      var x = 0;
+      var y = 0;
+      for(var plr in allPlayers){
+        //position the cars a little away from each other in x direction
+         x = x + 200; 
+         //use data form the database to display the cars in y direction 
+         y = displayHeight - allPlayers[plr].distance; 
+         cars[index-1].x = x; 
+         cars[index-1].y = y;
+         if(index === player.index){
+           car[index - 1].shapeColor = "red";
+           camera.position.x = displayWidth/2;
+           camera.position.y = cars[index - 1].y;
+         }
+        if (plr === "player" + player.index)
+          fill("red")
+        else
+          fill("black");
+
+        display_position+=20;
+        textSize(15);
+        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      }
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=50
+      player.update();
+    }
+  }
+}
